fc432c115dbe39ebd7671ab7d34ffd4fb94088ab
HopKaa <127760062+HopKaa@users.noreply.github.com>
2023-05-30 21:55:25 +0700

    added prime factorization

--- Assets/Scripts/Calculator.cs
+++ Assets/Scripts/Calculator.cs
@@ -13,7 +13,6 @@ public class Calculator : MonoBehaviour
 
     private float _value1;
     private float _value2;
-    private string _operation;
 
 
     public void ClickNumber(int value)
@@ -27,125 +26,135 @@ public class Calculator : MonoBehaviour
         else
         {
             _value2 += value;
-        }    
+        }
     }
     public void ClickOperation(string value)
     {
         Debug.Log($" ClickOperation value: {value}");
-        _operation += value;
         _text.text += $"{value}";
     }
 
-    public void ClickEqual(string value)
+    public void ClickPlus()
     {
-        Debug.Log($" ClickEqual value: {value}");
-        if (!string.IsNullOrEmpty(_operation))
+        ShowResult(_value1 + _value2);
+        UpdateText("+");
+    }
+
+    public void ClickMinus()
+    {
+        ShowResult(_value1 - _value2);
+        UpdateText("-");
+    }
+
+    public void ClickMultiplication()
+    {
+        ShowResult(_value1 * _value2);
+        UpdateText("*");
+    }
+
+    public void ClickDivisin()
+    {
+        if (_value2 != 0)
         {
-            switch (_operation)
+            ShowResult(_value1 / _value2);
+        }
+
+        if (_value2 == 0)
+        {
+            _text.text = "division by 0 is not possible";
+        }
+        UpdateText("/");
+    }
+
+    public void ClickDegree()
+    {
+        ShowResult(Mathf.Pow(_value1, _value2));
+        UpdateText("^");
+    }
+
+    public void ClickLog2()
+    {
+        ShowResult(Mathf.Log(_value1));
+        UpdateText("log2");
+    }
+
+    public void ClickLog10()
+    {
+        ShowResult(Mathf.Log10(_value1));
+        UpdateText("log10");
+    }
+
+    public void ClickMinimum()
+    {
+        ShowResult(Mathf.Min(_value1, _value2));
+        UpdateText("min");
+    }
+
+    public void ClickMaximum()
+    {
+        ShowResult(Mathf.Max(_value1, _value2));
+        UpdateText("max");
+    }
+
+    public void ClickCot()
+    {
+        if (Mathf.Tan(_value1) != 0)
+        {
+            ShowResult(1 / Mathf.Tan(_value1));
+        }
+
+        if (Mathf.Tan(_value1) == 0)
+        {
+            ShowResult(0);
+        }
+        ShowResult(1 / Mathf.Tan(_value1));
+        UpdateText("cot");
+    }
+
+    public void ClickSin()
+    {
+        ShowResult(Mathf.Sin(_value1));
+        UpdateText("sin");
+    }
+
+    public void ClickPI()
+    {
+        ShowResult(Mathf.PI);
+        UpdateText("");
+    }
+    public void OnCalculateClick()
+    {
+        int n = int.Parse(_text.text);
+        string result = "Prime factors of a number " + n + ": ";
+
+        for (int i = 2; i <= n; i++)
+        {
+            while (n % i == 0)
             {
-                case "+":
-                    ShowResult(_value1 + _value2);
-                    break;
-                case "-":
-                    ShowResult(_value1 - _value2);
-                    break;
-                case "*":
-                    ShowResult(_value1 * _value2);
-                    break;
-                case "/":
-                    if (_value2 != 0)
-                    {
-                        ShowResult(_value1 / _value2);
-                    }
-
-                    if (_value2 == 0)
-                    {
-                        _text.text = "Деление на 0 невозможно";
-                    }
-                    break;
-
-                case "log2":
-                    ShowResult(Mathf.Log(_value1));
-                    break;
-                case "log10":
-                    ShowResult(Mathf.Log10(_value1));
-                    break;
-                case "min":
-                    ShowResult(Mathf.Min(_value1, _value2));
-                    break;
-                case "max":
-                    ShowResult(Mathf.Max(_value1, _value2));
-                    break;
-                case "^":
-                    ShowResult(Mathf.Pow(_value1, _value2));
-                    break;
-                case "cot":
-                    if (Mathf.Tan(_value1) != 0)
-                    {
-                        ShowResult(1 / Mathf.Tan(_value1));
-                    }
-
-                    if (Mathf.Tan(_value1) == 0)
-                    {
-                        ShowResult(0);
-                    }
-                    ShowResult(1 / Mathf.Tan(_value1));
-                    break;
-                case "sin":
-                    ShowResult(Mathf.Sin(_value1));
-                    break;
-                case "pi":
-                    ShowResult(Mathf.PI);
-                    break;
-                case "izi":
-                    string result = "";
-                    for (; _value1 % 2 == 0; _text.text += $"{result}")
-                    {
-                        _value1 = _value1 / 2;
-                        ShowResultIzi(2);
-                    }
-
-                    for (; _value1 % 3 == 0; _text.text += $"{result}")
-                    {
-                        _value1 = _value1 / 3;
-                        ShowResultIzi(3);
-                    }
-
-                    for (; _value1 % 5 == 0; _text.text += $"{result}")
-                    {
-                        _value1 = _value1 / 5;
-                        ShowResultIzi(5);
-                    }
-
-                    for (; _value1 % 7 == 0; _text.text += $"{result}")
-                    {
-                        _value1 = _value1 / 7;
-                        ShowResultIzi(7);
-                    }
-
-                    for (; _value1 % 11 == 0; _text.text += $"{result}")
-                    {
-                        _value1 = _value1 / 11;
-                        ShowResultIzi(11);
-                    }
-                    break;
-                default:
-                    break;
+                result += i + " ";
+                n /= i;
             }
         }
+
+        _text.text = result;
+    }
+    public void ClickEqual(string operation)
+    {
+       _text.text += $"{operation}"; 
     }
 
     public void ClickClear()
     {
         _text.text = "";
     }
-
+    private void UpdateText(string operation)
+    {
+        _text.text += $"{operation}";
+    }
     private void ShowResult(float result)
     {
-        _text.text = "Result: " + result.ToString();
+        _text.text = result.ToString();
     }
-    private void ShowResultIzi(float result)
-    {
-        _text.text += $"{result}";
-    }
+
+   
 }
